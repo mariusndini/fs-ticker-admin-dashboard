35810eca4448920a6a7713227b3c42345d34cb64
Marius Ndini <mariusndini@yahoo.com>
2023-04-03 09:04:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import snowflake.connector
+import email.utils
+
+
 
 # CONNECT TO SNOWFLAKE  
 conn = snowflake.connector.connect( user= st.secrets["user"],
@@ -17,6 +20,27 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
+def check_email(e):
+    msg = email.message_from_string(data[0][1])
+    addr = email.utils.parseaddr(msg['From'])[1]
+    domain = addr.split('@')[1]
+    if domain == "example.com":
+        return True
+    else:
+        return False
 
 st.title('Ticker Admin Dashboard')
 st.text('Please use the inputs below to gain access to the Ticker Snowflake account.')
+
+text_input = st.text_input(
+        "Enter your SNOWFLAKE E-Mail👇",
+        label_visibility = st.session_state.visibility,
+        disabled = st.session_state.disabled,
+        placeholder = "Only Valid Snowflake E-Mails Allowed",
+    )
+
+if st.button('GO!'):
+    st.write(text_input)
+
+
+
